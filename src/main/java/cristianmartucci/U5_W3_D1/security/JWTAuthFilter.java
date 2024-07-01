9f76daae417dfadf461efa51bd273c60de9d7964
CristianMartu <martuccicristian18@gmail.com>
2024-07-01 16:38:40 +0200

    commit

--- src/main/java/cristianmartucci/U5_W3_D1/security/JWTAuthFilter.java
+++ src/main/java/cristianmartucci/U5_W3_D1/security/JWTAuthFilter.java
@@ -5,12 +5,15 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JWTAuthFilter extends OncePerRequestFilter {
     @Autowired
     private JWTTools jwtTools;
